76f214f3c6c68623f564ce830acd8f3b943d3ea6
Terry Bates <terryjbates@gmail.com>
2015-01-31 23:23:41 -0800

    Added point.py.

---
+++ point.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+"""
+point.py
+
+Created by Terry Bates on 2015-01-31.
+Copyright (c) 2015 http://the-awesome-python-blog.posterous.com. All rights reserved.
+"""
+
+class Point:
+    pass
+    
+p1 = Point()
+p2 = Point()
+
+p1.x = 5
+p1.y = 4
+
+p2.x = 3
+p2.y = 6
+
+print(p1.x, p1.y)
+print(p2.x, p2.y)
