35b69e3a77a5b2b70181a06f044cc768295b6ce7
Terry Bates <terryjbates@gmail.com>
2015-02-21 20:17:30 -0800

    Added script to use optparse module.

--- opts.py
+++ opts.py
@@ -1,1 +1,14 @@
+#!/usr/bin/env python3
+
 __author__ = 'tbates'
+from optparse import OptionParser
+
+def main():
+    parser = OptionParser()
+    parser.add_option("-f", "--file", dest="filename",
+                            help="write report to FILE", metavar="FILE")
+    (options, args) = parser.parse_args()
+
+    print("options:", options)
+    print("arguments:", args)
+main()
