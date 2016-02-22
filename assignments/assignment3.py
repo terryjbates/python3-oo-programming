c838fce2113e76035f7e08f070efbc98ca527601
Terry Bates <terryjbates@gmail.com>
2016-02-22 00:55:57 -0800

    Removed more print statements. Removed try/except for simple os test. Added modified split param to take first '=' to split only once.

--- assignments/assignment3.py
+++ assignments/assignment3.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python3
-
+import os
 
 class ConfigDict(dict):
 
     def __init__(self, config_file):
         #super(ConfigDict, self).__init__(self)
-        self.config_file = config_file
-        try:
+        if os.path.exists(config_file):
+            self.config_file = config_file
             with open(self.config_file, 'r') as fh:
                 # http://bit.ly/1oWKAxc
                 all_lines = (line.rstrip() for line in fh)
                 non_blank_lines = (line for line in all_lines if line)
                 for line in non_blank_lines:
                     line = line.strip()
-                    (key, value) = line.split("=")
+                    (key, value) = line.split("=", 1)
                     #print("{}:{}".format(key, value))
                     self.__setitem__(key, value)
             self.write_dict_to_file()
-        except Exception as ex:
-            print("Import failed due to: {}".format(ex))
+        else:
+            print("Import failed due to file {} not existing".format(config_file))
 
     def __setitem__(self, key, value):
-        print("key:{}  value:{}".format(key, value))
+        #print("key:{}  value:{}".format(key, value))
         dict.__setitem__(self, key, value)
         self.write_dict_to_file()
 
