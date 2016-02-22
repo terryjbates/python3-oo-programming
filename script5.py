eeeca2b3d563afffb2a37b846c96ecf4f0c48952
Terry Bates <terryjbates@gmail.com>
2016-02-21 22:22:16 -0800

    Added silly script seemingly meant to read the tart of a file.

--- script5.py
+++ script5.py
@@ -1,1 +1,13 @@
-__author__ = 'tbates'
+#!/usr/bin/env python3
+# __author__ = 'tbates'
+
+import fileinput
+
+def main():
+    for line in fileinput.input():
+        if fileinput.isfirstline():
+            print("<start of file {0}>".format(fileinput.filename()))
+        print(line.strip())
+
+if __name__ == '__main__':
+    main()
