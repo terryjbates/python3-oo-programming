c49214c1303cec776b387c2d2e8a611d1017d1f5
Terry Bates <tbates@linkedin.com>
2016-02-21 21:50:21 -0800

    Elaborate example of object inheritance lookup hierarchy.

---
+++ inheritance_examples.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import abc
+
+class GetSetParent(object):
+    __metaclass__ = abc.ABCMeta
+    def __init__(self, value):
+        self.val = 0
+    def set_val(self, value):
+        self.val = value
+    def get_val(self):
+        return self.val
+    @abc.abstractmethod
+    def showdoc(self):
+        return
+
+class GetSetInt(GetSetParent):
+    def set_val(self, value):
+        if not isinstance(value, int):
+            value = 0
+        super(GetSetInt, self).set_val(value)
+    def show(self):
+        print('GetSetInt object ({0}), only acceptis '
+              'integer values'.format(id(self)))
+
+class GetSetList(GetSetParent):
+    def __init__(self, value=0):
+        self.vallist = [value]
+    def get_val(self):
+        return self.vallist[-1]
+    def get_vals(self):
+        return self.vallist
+    def set_val(self, value):
+        self.vallist.append(value)
+    def showdoc(self):
+        print('GetSetList object, len {0} '
+              'history of values set'.format(len(self.vallist)))
+
+
+x = GetSetInt(3)
+x.set_val(5)
+print(x.get_val())
+x.showdoc()
+
+gsl = GetSetList(5)
+gsl.set_val(10)
+gsl.set_val(20)
+print(gsl.get_val())
+print(gsl.get_vals())
+gsl.showdoc()
