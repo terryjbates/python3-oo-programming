020e831a39f6d53d74f39635857058e1bfda8125
Terry Bates <tbates@linkedin.com>
2016-07-13 00:27:28 -0700

    Created basic TCP Timestamp client and server.

---
+++ core-python/ch_16/tsTserv.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from random import randint
+from socket import *
+from time import ctime
+
+HOST = ''
+PORT = 2437
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+tcpSerSock = socket(AF_INET, SOCK_STREAM)
+tcpSerSock.bind(ADDR)
+tcpSerSock.listen(5)
+
+while True:
+    print("Waiting for connection on {}".format(PORT))
+    tcpCliSock, addr = tcpSerSock.accept()
+    print("...connected from: {}".format(addr))
+
+    while True:
+        data = tcpCliSock.recv(BUFSIZ)
+        if not data:
+            break
+        data = str(data, 'utf-8')
+        echo_data = "{} {}".format(ctime(), data)
+        tcpCliSock.send(bytes(echo_data, "utf-8"))
+    tcpCliSock.close()
+tcpSerSock.close()
