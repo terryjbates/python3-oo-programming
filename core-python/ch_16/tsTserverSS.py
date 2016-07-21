7296e9c3ea8aad13707a24a0ef1ed98c5900934f
Terry Bates <terryjbates@gmail.com>
2016-07-21 02:21:10 -0700

    Pretty print self.client_address, make server port reusable after restart

--- core-python/ch_16/tsTserverSS.py
+++ core-python/ch_16/tsTserverSS.py
@@ -3,6 +3,8 @@ from socketserver import (TCPServer as TCP,
                           StreamRequestHandler as SRH)
 
 from time import ctime
+import pprint
+
 
 HOST = ''
 PORT = 2437
@@ -14,6 +16,7 @@ class MyRequestHandler(SRH):
     def handle(self):
         # self.request is the TCP socket connected to the client
         data = self.request.recv(1024).strip()
+        print("{}".format(self.client_address))
         print("{} wrote:".format(self.client_address[0]))
         print(data)
         data = str(data, 'utf-8')
@@ -21,5 +24,7 @@ class MyRequestHandler(SRH):
         self.request.sendall(bytes(echo_data, 'utf-8'))
 
 tcpServ = TCP(ADDR, MyRequestHandler)
+# https://brokenbad.com/address-reuse-in-pythons-socketserver/
+tcpServ.allow_reuse_address = True
 print("...waiting for connection")
 tcpServ.serve_forever()
