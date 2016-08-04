30bdc301f9370dcd3e8d69f523cde0726e53836c
Terry Bates <tbates@linkedin.com>
2016-08-04 14:00:37 -0700

    Add code to get file length, separate process logic into separate file

--- core-python/ch_18/ex_18_4.py
+++ core-python/ch_18/ex_18_4.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 import sys
 import os
+from myThread import MyThread
+
+
+NUM_OF_THREADS = 4
 
 
 def findall(p, s):
@@ -12,6 +16,17 @@ def findall(p, s):
         i = s.find(p, i+1)
 
 
+def process_file(filename)
+    print("Total file size in bytes: {}".format(total_size))
+    with open(filename, 'r') as f:
+        for index, line in enumerate(f):
+            for i in findall(word_to_search, line):
+                print(index + 1, i)
+                print(line.strip())
+                print()
+            #print(line.strip())
+
+
 def main():
     try:
         print(sys.argv[1])
@@ -21,14 +36,23 @@ def main():
     except IndexError:
         print("Usage: ex_18_4.py <filename> <word>")
         sys.exit()
+    total_size = os.path.getsize(filename)
+    funcs = [process_file]
+    nfuncs = range(len(funcs))
+    threads = []
+    for i in nfuncs:
+        t = MyThread(funcs[i], (q, nloops),
+            funcs[i].__name__)
+        threads.append(t)
 
-    with open(filename, 'r') as f:
-        for index, line in enumerate(f):
-            for i in findall(word_to_search, line):
-                print(index + 1, i)
-                print(line.strip())
-                print()
-            #print(line.strip())
+    for i in nfuncs:
+        threads[i].start()
+
+    for i in nfuncs:
+        threads[i].join()
+
+
+
 
 
 if __name__ == "__main__":
